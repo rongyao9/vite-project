ff0fb7345fe61d883f8154c50e475b3082d1e42c
dengrongyao <dengrongyao9@163.com>
2021-04-09 17:41:29 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { buildConfig } from './buildConfig';
 export default defineConfig({
-  plugins: [vue()]
+  server: {
+    // port: 7874,
+    proxy: {
+      // '/lsbdb': 'http://10.192.195.96:8087',
+    },
+    hmr: {
+      overlay: true,
+    },
+    open: true,
+  },
+  build: buildConfig,
+  alias: {
+    '/@': path.resolve(__dirname, '.', 'src'),
+  },
+  optimizeDeps: {
+    include: [],
+  },
+  plugins: [vue(), vueJsx()],
 })
